0706a9085ced83876ae7e99937945d7add3eaa33
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-10-17 15:28:49 +0200

    change newUpdaterEvent to take v1.Service as input and handle annotaiotns on service

--- updater.go
+++ updater.go
@@ -4,20 +4,32 @@ import (
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
 )
 
 const (
-	updaterEventdCreate = "create"
-	updaterEventUpdate  = "update"
-	updaterEventDelete  = "delete"
+	eventCreate       = "create"
+	eventUpdate       = "update"
+	eventDelete       = "delete"
+	serviceAnnotation = "zookeeper/path"
 )
 
-func newUpdaterEvent(eventType string, member *zkMember) *UpdaterEvent {
+func newUpdaterEvent(eventType string, service *v1.Service) (*UpdaterEvent, error) {
+	member := newZKMember()
+	member.name = service.GetName()
+
+	annotations := service.GetAnnotations()
+	if path, ok := annotations[serviceAnnotation]; ok {
+		member.path = path
+	} else {
+		return nil, fmt.Errorf("failed to find service annotation: %v on service: %v", serviceAnnotation, member.name)
+	}
+
 	event := UpdaterEvent{
 		eventType: eventType,
 		member:    member,
 	}
-	return &event
+	return &event, nil
 }
 
 // UpdaterEvent create/update/delete of zkmember
