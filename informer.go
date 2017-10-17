8a486347dcc6f695a021cb9c358dccb4038c9e74
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-10-17 15:32:01 +0200

    change informer to not use worker queue, adding initial functionallity to handle events for create/update/delete to send to updater chan

--- informer.go
+++ informer.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
-	"github.com/golang/glog"
+	log "github.com/sirupsen/logrus"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	lister_v1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/workqueue"
 )
 
 func k8sGetClientConfig(kubeconfig string) (*rest.Config, error) {
@@ -43,15 +39,16 @@ func k8sGetClient(kubeconfig string) (*kubernetes.Clientset, error) {
 type serviceController struct {
 	client        kubernetes.Interface
 	informer      cache.Controller
-	queue         workqueue.RateLimitingInterface
+	indexer       cache.Indexer
 	serviceLister lister_v1.ServiceLister
+	updater       *Updater
 }
 
 func newServiceController(client kubernetes.Interface, namespace string, updateInterval time.Duration) *serviceController {
 	sc := &serviceController{
 		client: client,
-		queue:  workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
 	}
+	sc.updater = newUpdater()
 
 	indexer, informer := cache.NewIndexerInformer(
 		&cache.ListWatch{
@@ -64,7 +61,7 @@ func newServiceController(client kubernetes.Interface, namespace string, updateI
 			},
 		},
 		// The types of objects this informer will return
-		&v1.ServiceList{},
+		&v1.Service{},
 		// The resync period of this object. This will force a re-queue of all cached objects at this interval.
 		// Every object will trigger the `Updatefunc` even if there have been no actual updates triggered.
 		// In some cases you can set this to a very high interval - as you can assume you will see periodic
@@ -74,17 +71,39 @@ func newServiceController(client kubernetes.Interface, namespace string, updateI
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
 				if key, err := cache.MetaNamespaceKeyFunc(obj); err == nil {
-					sc.queue.Add(key)
+					log.Debugf("addFunc key: %v", key)
+					service := obj.(*v1.Service)
+					event, err := newUpdaterEvent(eventCreate, service)
+					if err != nil {
+						log.Debugf("failed to generate new updater event: %v", err.Error())
+					} else {
+						sc.updater.events <- *event
+					}
+
 				}
 			},
 			UpdateFunc: func(old, new interface{}) {
 				if key, err := cache.MetaNamespaceKeyFunc(new); err == nil {
-					sc.queue.Add(key)
+					log.Debugf("updateFunc key: %v", key)
+					service := new.(*v1.Service) // TODO: do we need to do a diff on old/new?
+					event, err := newUpdaterEvent(eventUpdate, service)
+					if err != nil {
+						log.Debugf("failed to generate new updater event: %v", err.Error())
+					} else {
+						sc.updater.events <- *event
+					}
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
 				if key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj); err == nil {
-					sc.queue.Add(key)
+					log.Debugf("deleteFunc key: %v", key)
+					service := obj.(*v1.Service)
+					event, err := newUpdaterEvent(eventUpdate, service)
+					if err != nil {
+						log.Debugf("failed to generate new updater event: %v", err.Error())
+					} else {
+						sc.updater.events <- *event
+					}
 				}
 			},
 		},
@@ -92,87 +111,18 @@ func newServiceController(client kubernetes.Interface, namespace string, updateI
 	)
 
 	sc.informer = informer
+	sc.indexer = indexer
 	sc.serviceLister = lister_v1.NewServiceLister(indexer)
 
 	return sc
 }
 
 func (c *serviceController) Run(stopCh chan struct{}) {
-	defer c.queue.ShutDown()
-	glog.Info("Starting serviceController")
+	log.Info("Starting serviceController")
 
 	go c.informer.Run(stopCh)
-
-	// Wait for all caches to be synced, before processing items from the queue is started
-	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
-		glog.Error(fmt.Errorf("Timed out waiting for caches to sync"))
-		return
-	}
-
-	// Launching additional goroutines would parallelize workers consuming from the queue (but we don't really need this)
-	go wait.Until(c.runWorker, time.Second, stopCh)
+	go c.updater.Run(stopCh)
 
 	<-stopCh
-	glog.Info("Stopping serviceController")
-}
-
-func (c *serviceController) runWorker() {
-	for c.processNext() {
-	}
-}
-
-func (c *serviceController) processNext() bool {
-	// Wait until there is a new item in the working queue
-	key, quit := c.queue.Get()
-	if quit {
-		return false
-	}
-
-	// Tell the queue that we are done with processing this key. This unblocks the key for other workers
-	// This allows safe parallel processing because two pods with the same key are never processed in
-	// parallel.
-	defer c.queue.Done(key)
-	// Invoke the method containing the business logic
-	err := c.process(key.(string))
-	// Handle the error if something went wrong during the execution of the business logic
-	c.handleErr(err, key)
-	return true
-}
-
-func (c *serviceController) process(key string) error {
-	selector := labels.NewSelector()
-
-	services, err := c.serviceLister.List(selector)
-	if err != nil {
-		return fmt.Errorf("failed to retrieve node by key %q: %v", key, err)
-	}
-
-	for _, service := range services {
-		fmt.Printf("%v %v\n", service.GetName(), service.GetNamespace())
-	}
-
-	return nil
-}
-
-func (c *serviceController) handleErr(err error, key interface{}) {
-	if err == nil {
-		// Forget about the #AddRateLimited history of the key on every successful synchronization.
-		// This ensures that future processing of updates for this key is not delayed because of
-		// an outdated error history.
-		c.queue.Forget(key)
-		return
-	}
-
-	// This controller retries 5 times if something goes wrong. After that, it stops trying.
-	if c.queue.NumRequeues(key) < 5 {
-		glog.Infof("Error processing %v: %v", key, err)
-
-		// Re-enqueue the key rate limited. Based on the rate limiter on the
-		// queue and the re-enqueue history, the key will be processed later again.
-		c.queue.AddRateLimited(key)
-		return
-	}
-
-	c.queue.Forget(key)
-	glog.Errorf("Dropping %q out of the queue: %v", key, err)
+	log.Info("Stopping serviceController")
 }
